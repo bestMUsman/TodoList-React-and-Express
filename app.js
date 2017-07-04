596c2f7a0a95cd98d0ca5b38d0a88c00c9a2cabf
Muhammad Usman <musmanraodev@gmail.com>
2017-07-04 15:55:05 -0400

    updated app.js for heroku

--- app.js
+++ app.js
@@ -1,29 +1,33 @@
-const express = require('express');
-const path = require('path');
-const logger = require('morgan');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 
-
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, function () {
-    console.log(`listening on port ${PORT}`);
+app.listen(PORT, function() {
+  console.log(`listening on port ${PORT}`);
 });
 
-app.use('/static', express.static(path.join(__dirname, 'public')));
+// app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "client/build")));
 app.use(cors());
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/public/index.html');
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/public/index.html");
 });
 
-const todolistRoutes = require('./routes/todolistRoute');
-app.use('/api/todolist', todolistRoutes);
+const todolistRoutes = require("./routes/todolistRoute");
+app.use("/api", todolistRoutes);
 
-app.get('*', function (req, res) {
-    res.status(404).send({ message: 'Not Found, Sorry :)' });
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+
+app.get("*", function(req, res) {
+  res.status(404).send({ message: "Not Found, Sorry :)" });
 });
