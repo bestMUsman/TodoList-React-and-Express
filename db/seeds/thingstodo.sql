fbdf21ea461dd483da79e5a03d0ca41350a4c27d
Muhammad Usman <musmanraodev@gmail.com>
2017-10-10 23:56:59 -0400

    added line through ability and fixed empty input

--- db/seeds/thingstodo.sql
+++ db/seeds/thingstodo.sql
@@ -4,6 +4,6 @@ INSERT INTO thingstodo (content, checked) VALUES
   ('Be A Kind Person', FALSE),
   ('Learn Programming!', FALSE),
   ('Improve Yourself', FALSE),
-  ('Finish the Homework', FALSE),
+  ('Finish the Homework', TRUE),
   ('Pure Your Thoughts', FALSE),
-  ('Clean Your Heart!', TRUE);
+  ('Clean Your Heart!', FALSE);
