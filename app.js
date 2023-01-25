a0d927f3d24b5294d23c54f5705f7b8ab683415f
Muhammad Usman <musmanraodev@gmail.com>
2023-01-24 21:52:35 -0500

    updated for onrender

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require("path");
 const logger = require("morgan");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+require("dotenv").config();
+
 const app = express();
 
 const PORT = process.env.PORT || 3001;
@@ -24,7 +26,9 @@ app.use(express.static(path.join(__dirname, "client/build")));
 
 const todolistRoutes = require("./routes/todolistRoute");
 app.use("/api/todolist", todolistRoutes);
-
+app.get("/api", function(req, res) {
+  res.send('working');
+});
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
--- db/config.js
+++ db/config.js
@@ -1,33 +1,15 @@
-// setting up a variable that logs every query
-const options = {
-  connect: (client, dc, isFresh) => {
-    // do this every time the database connects
-    console.log('Connected to database:', client.connectionParameters.database)
-  },
-  query: (e) => {
-    // do this every time a query is made to the database
-    console.log('making query ========> ' + e.query);
-  },
-  receive: (data, result, e) => {
-    // do this every time the app receives data from the database
-    console.log('completed query =======> ' + e.query);
-  },
-  disconnect: (client, dc) => {
-    // do this every time the database disconnects (usually after every query)
-    console.log('Disconnecting from database:', client.connectionParameters.database);
-  }
-};
-
-const pgp = require('pg-promise')(options);
+const pgp = require('pg-promise')();
 
 let db;
 
 if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
-  db = pgp({
-    database: 'todolist_development',
-    port: 5432,
-    host: 'localhost',
-  });
+  db = pgp(process.env.DATABASE_URL);
+
+//   db = pgp({
+//     database: 'ebay_fav_items',
+//     port: 5432,
+//     host: 'localhost',
+//   });
 } else if (process.env.NODE_ENV === 'production') {
   db = pgp(process.env.DATABASE_URL);
 }
